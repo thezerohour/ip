a447b61ce06657c3f810025c3111aa6d3fe83e42
Piraveen Alexander <e1122425@u.nus.edu>
2025-01-23 01:56:38 +0800

    rename Duke to Pixy

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Pixy User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Pixy.java
@@ -0,0 +1,7 @@
+public class Pixy {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Pixy");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
