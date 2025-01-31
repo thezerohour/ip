38d83cda7ea6f63b86c585d01b64a45181c9fe4f
Piraveen Alexander <e1122425@u.nus.edu>
2025-01-31 12:51:05 +0800

    implement command echo

--- src/main/java/Pixy.java
+++ src/main/java/Pixy.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class Pixy {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
         System.out.println("Hello! I'm Pixy");
         System.out.println("What can I do for you?");
+        String command = in.nextLine();
+        while (!command.equals("bye")) {
+            System.out.println(command);
+            command = in.nextLine();
+        }
         System.out.println("Bye. Hope to see you again soon!");
+
     }
 }
