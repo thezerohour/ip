054d37f597bba52c6073f0f8e4249c3602c054a3
Piraveen Alexander <e1122425@u.nus.edu>
2025-01-31 14:25:18 +0800

    create Task class, mark and unmark commands

--- src/main/java/Pixy.java
+++ src/main/java/Pixy.java
@@ -3,21 +3,35 @@ import java.util.Scanner;
 public class Pixy {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String[] items = new String[100];
+        Task[] tasks = new Task[100];
 
         System.out.println("Hello! I'm Pixy");
         System.out.println("What can I do for you?");
         String command = in.nextLine();
-        int counter = 0;
+        int tasksCounter = 0;
         while (!command.equals("bye")) {
             if (command.equals("list")) {
-                for (int i = 0; i < counter; i++) {
-                    System.out.println((i + 1) + ". " + items[i]);
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < tasksCounter; i++) {
+                    System.out.println((i + 1) + ".[" + tasks[i].getStatusIcon() + "] " + tasks[i].description);
                 }
-            } else {
+            }
+            else if (command.contains("unmark")) {
+                int taskIndex = Integer.parseInt(command.substring(7));
+                tasks[taskIndex - 1].setDone(false);
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println((taskIndex) + ".[" + tasks[taskIndex - 1].getStatusIcon() + "] " + tasks[taskIndex - 1].description);
+            }
+            else if (command.contains("mark")) {
+                int taskIndex = Integer.parseInt(command.substring(5));
+                tasks[taskIndex - 1].setDone(true);
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println((taskIndex) + ".[" + tasks[taskIndex - 1].getStatusIcon() + "] " + tasks[taskIndex - 1].description);
+            }
+            else {
                 System.out.println("added: " + command);
-                items[counter] = command;
-                counter++;
+                tasks[tasksCounter] = new Task(command);
+                tasksCounter++;
             }
             command = in.nextLine();
         }
